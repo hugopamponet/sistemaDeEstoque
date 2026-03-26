102de9ba70c5c7005d7e1e63567ddf0a1da1b886
hugopamponet <hugofarias9@gmail.com>
2026-03-26 18:43:51 -0300

    Criando o js para consumir a API via CEP

--- src/main/java/connection/ConnectionFactory.java
+++ src/main/java/connection/ConnectionFactory.java
@@ -1,17 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package connection;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.sql.DriverManager;
-//https://codeshare.io/GqYRBA
-/**
- *
- * @author EJADEN0058
- */
+
 public class ConnectionFactory {
 
     //Dados da conexão
