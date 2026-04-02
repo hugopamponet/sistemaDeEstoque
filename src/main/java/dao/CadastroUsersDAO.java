4d67175d5db0a74606ed65643399b4b6c7a2cea9
hugopamponet <hugofarias9@gmail.com>
2026-04-01 21:45:38 -0300

    Criação do docker compose e dockerfile

--- src/main/java/dao/CadastroUsersDAO.java
+++ src/main/java/dao/CadastroUsersDAO.java
@@ -2,40 +2,45 @@ package dao;
 
 import connection.ConnectionFactory;
 import java.sql.PreparedStatement;
-import java.util.ResourceBundle;
 import model.CadastroUsuarioModel;
 
 public class CadastroUsersDAO {
     
     public boolean cadastrar(CadastroUsuarioModel user) {
         String sql = "INSERT INTO users " + 
-                "(username, psw, nameFirst, sobrenome, matricula, cpf, sexo, dtaNascimento, email, telefone, funcao, cep, endereco, numero, bairro, cidade, estado, complemento) " +
-                "VALUE(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-        
-        try (var con = ConnectionFactory.getConnection()) {
-            
-            PreparedStatement stmt = con.prepareStatement(sql);
-            
-            stmt.setString(1, user.getNome());
-            stmt.setString(2, user.getSobrenome());
-            stmt.setString(3, user.getCpf());
-            stmt.setString(4, user.getFuncao());
-            stmt.setString(5, user.getCep());
-            stmt.setString(6, user.getEndereco());
-            stmt.setString(7, user.getBairro());
-            stmt.setString(8, user.getCidade());
-            stmt.setString(9, user.getEstado());
-            stmt.setString(10, user.getComplemento());
-            stmt.setLong(11, user.getNumero());
-            stmt.setString(12, user.getNomeUsuario());
-            stmt.setString(13, user.getSenha());
-            
-            stmt.executeUpdate();
-            
-            return true;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
+"(username, psw, nameFirst, sobrenome, matricula, cpf, sexo, dtaNascimento, email, telefone, funcao, cep, endereco, numero, bairro, cidade, estado, complemento) " +
+"VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+try (var con = ConnectionFactory.getConnection()) {
+
+    PreparedStatement stmt = con.prepareStatement(sql);
+
+    stmt.setString(1, user.getNomeUsuario());   // username
+    stmt.setString(2, user.getSenha());         // psw
+    stmt.setString(3, user.getNome());          // nameFirst
+    stmt.setString(4, user.getSobrenome());
+    stmt.setString(5, user.getMatricula());
+    stmt.setString(6, user.getCpf());
+    stmt.setString(7, user.getSexo());
+    stmt.setString(8, user.getDtaNascimento());
+    stmt.setString(9, user.getEmail());
+    stmt.setString(10, user.getTelefone());
+    stmt.setString(11, user.getFuncao());
+    stmt.setString(12, user.getCep());
+    stmt.setString(13, user.getEndereco());
+    stmt.setLong(14, user.getNumero());
+    stmt.setString(15, user.getBairro());
+    stmt.setString(16, user.getCidade());
+    stmt.setString(17, user.getEstado());
+    stmt.setString(18, user.getComplemento());
+
+    stmt.executeUpdate();
+
+    return true;
+
+} catch (Exception e) {
+    e.printStackTrace();
+    return false;
+}
     }
 }
