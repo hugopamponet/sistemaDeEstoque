1475382b700ab47a21b7e31d85f306ac1fe8b397
HUGO PAMPONET FARIAS <EJADEN0058@universidadesenaicimatec.edu.br>
2026-04-11 12:02:08 -0300

    Alteração do docker compose

--- src/main/java/connection/ConnectionFactory.java
+++ src/main/java/connection/ConnectionFactory.java
@@ -2,25 +2,27 @@ package connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionFactory {
-
-    //Dados da conexão
+        private static final String URL = ("DB_URL");
+        private static final String USER = ("DB_USER");
+        private static final String PASSWORD = ("DB_PASS");
         private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
-        private static final String URL =
-            "jdbc:mysql://db:3306/estoque_db?usesSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
-        private static final String USER = "root";
-        private static final String PASSWORD = "cimatec";
-    //Métodos
-        
+
         public static Connection getConnection(){
             Connection con = null;
             
             try {
                 Class.forName(DRIVER);
+
                 con = DriverManager.getConnection(URL, USER, PASSWORD);
                 System.out.println("Banco de dados conectado.");
-            } catch (Exception e) {
+
+            }catch (SQLException e) {
+                System.out.println("Erro no SQL");
+                e.printStackTrace();
+            }catch (Exception e) {
                 System.out.println("Banco de dados não conectado");
                 e.printStackTrace();
             }
